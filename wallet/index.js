97be7a19543c383002c29a8ad4c20b6d829d709d
@ivelton2018 <ivelton2018@gmail.com>
2018-11-09 09:38:19 -0400

    wallet class

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,17 @@
-class Wallet {
-	constructor(){
+const { INITIAL_BALANCE } = require('../config');	
 
+class Wallet{
+	constructor(){
+		this.balance = INITIAL_BALANCE;
+		this.keyPair = null;
+		this.publicKey = null;
+	}
+
+	toString(){
+		return `Wallet -
+				public key 	: ${this.publicKey.toString()}
+				balance 		: #{this.balance}`
 	}
 }
+
+module.exports = Wallet;
