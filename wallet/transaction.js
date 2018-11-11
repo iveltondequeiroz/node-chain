f4935896c1252eab1b30782879142aa6ccee9f97
@ivelton2018 <ivelton2018@gmail.com>
2018-11-11 12:34:40 -0400

    verify transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -33,6 +33,17 @@ class Transaction {
 			signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
 		}
 	}
+
+	static verifyTransaction(transaction){
+
+		return ChainUtil.verifySignature(
+			transaction.input.address, 
+			transaction.input.signature, 
+			ChainUtil.hash(transaction.outputs)
+		
+	}
+
+
 }
 
 module.exports = Transaction;
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -22,6 +22,11 @@ describe('Transaction', () =>{
 		.toEqual(amount);
 	});
 
+	it('inputs the balance of the wallet', ()=>{
+		expect(transaction.input.amount).toEqual(wallet.balance);
+	});
+
+
 	describe('transacting with amount that exceeds the balance', () =>{
 		beforeEach(()=> {
 			amount = 50000;
