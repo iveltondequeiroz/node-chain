99cb1dadbe1a17f965abceba8bc9cdbfbdf6fe94
@ivelton2018 <ivelton2018@gmail.com>
2018-11-12 20:03:01 -0400

    test valid and corrupt transactions

--- app/miner.js
+++ app/miner.js
@@ -1,6 +1,19 @@
 class Miner {
-	constructor(blockchain, transactionPool){
+	constructor(blockchain, transactionPool, wallet, p2pServer){
 		this.blockchain = blockchain;
 		this.transactionPool = transactionPool;
+		this.wallet = wallet;
+		this.p2pServer = p2pServer;
+	}
+
+	mine(){
+		const validTransactions = this.transactionPool.validTransactions();
+		// include a reward
+		// creat a valid transactions block
+		// sync chains on p2p server
+		// clear transaction pool
+		// broadcast to every miner to clear their transaction pool
 	}
 }
+
+module.exports = Miner;
