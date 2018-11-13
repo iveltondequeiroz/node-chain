4bfbb87fee01b7399c730f9884fa47da9642b29b
@ivelton2018 <ivelton2018@gmail.com>
2018-11-12 20:39:13 -0400

    mining reward test

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,6 +1,6 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
-
+const { MINING_REWARD } = require('../config');	
 describe('Transaction', () =>{
 	let transaction, wallet, recipient, amount; 
 
@@ -58,7 +58,18 @@ describe('Transaction', () =>{
 		})
 		it('outputs an amount for the next recipient', ()=>{
 			expect(transaction.outputs.find(output => output.address === nextRecipient).amount).toEqual(nextAmount);
+		});		
+	});
+
+	describe('creating a reward transaction', ()=>{
+		beforeEach(()=> {
+			transaction = Transaction.rewardTransaction(wallet, Wallet.blockchainWallet());
 		});
 		
+		it(`reward the miners's wallet`, ()=>{
+			expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+			.toEqual(MINING_REWARD);
+		});
 	});
+
 });
